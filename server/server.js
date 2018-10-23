e48197666bcb22e2945b46bc462d2dfc5e2d7827
Cleber Santos <oclebersantos@gmail.com>
2018-10-23 00:50:53 -0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('./config/mongoose');
+const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
+const db = mongoose();
+const app = express();
+
+app.use('*', cors());
+
+const userSchema = require('./graphql/index').userSchema;
+
+app.use('/graphql', cors(), graphqlHTTP({
+	schema, userSchema,
+	rootValue: global,
+	graphiql: true
+}));
+
+app.listen(process.env.PORT || 4000, () => {
+	console.log('O graphQL está rodando na porta 4000');
+});
+
