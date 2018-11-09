9c7a5552074fe5f1c1cb90d0df165f9e13f38015
Cleber Santos <oclebersantos@gmail.com>
2018-11-09 16:12:23 -0200

    criando crud angular

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+// Apollo
+import { GraphQLModule } from './graphql.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule, // provides HttpClient for HttpLink
+    GraphQLModule,  // Import GraphQLModule
+    FormsModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
